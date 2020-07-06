1be7e66e7b6038f43c82382629c80d3b1be8f33b
daum1021@daum.net <41097199+danial1021@users.noreply.github.com>
2020-07-06 19:28:22 +0900

    add pop
    add pop

--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -5,9 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { // state는 변수를 의미
-    drawer: false
+    drawer: false,
+    attr: {
+      act: false,
+      msg: '',
+      color: 'error'
+    },
+    user: {
+      name: '사용자',
+      id: '',
+      img: 'https://randomuser.me/api/portraits/men/85.jpg'
+    }
   },
   mutations: { // mutations는 변수를 조작하는 함수를 의미
+    pop (state, s) {
+      state.attr.msg = s.msg
+      state.attr.color = s.color
+      state.attr.act = false
+      if (s.act === undefined) state.attr.act = true
+    }
   },
   actions: {
   },
