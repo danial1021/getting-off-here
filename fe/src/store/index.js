76266f33c16569eb97cb8e1ded56bbdab4a421bb
daum1021@daum.net <41097199+danial1021@users.noreply.github.com>
2020-07-14 16:17:04 +0900

    Access-Control-Allow-Origin(CORS)
    npm install cors in backend
    (Be scheduled)
    
    modify pagetitle components

--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
+axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+
 export default new Vuex.Store({
   state: { // state는 변수를 의미
     drawer: false,
@@ -46,7 +50,7 @@ export default new Vuex.Store({
     login({commit}, user){
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        axios({url: 'http://localhost:3000/login', data: user, method: 'POST' })
+        axios({url: 'login', data: user, method: 'POST' })
         .then(resp => {
           const token = resp.data.token
           const user = resp.data.user
