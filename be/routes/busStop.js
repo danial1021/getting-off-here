cb722c331ecc2d8c59efc244a576d6276f7ecee4
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-08-24 15:13:09 +0900

    정류장 검색

--- be/routes/busStop.js
+++ be/routes/busStop.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var path = require("path")
 var router = express.Router();
+const Busstop = require("../model/busStop")
+
+//정류장 목록 검색
+
+router.get('/', (req, res, next)=>{ //busstop_name 변수로 정류장 이름 보내주세요
+    Busstop.find({busstop_name:{$regex:req.body.busstop_name}})
+    .then((bus)=>{
+        console.log(bus[0])
+    })
+})
+
+
 
 module.exports = router;
