8cfc4b074e149b184e6d82478ec0ebcf9f6aaed6
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-08-24 14:58:19 +0900

    Update arrive.js

--- be/routes/arrive.js
+++ be/routes/arrive.js
@@ -7,7 +7,7 @@ const request = require('request');
 const HOST = 'http://api.gwangju.go.kr/xml/arriveInfo'
 const SERVICE_KEY = 'tEA2cLH8Amh%2FxeavdbPj1Kz0%2FL4bC6eYyo%2BbF2V8A28VE7%2FFl%2F%2Fsyd9OnPL03g40YafaSbb7rYYPqYs0vhd49A%3D%3D'
 
-router.get('/', (req, res, next)=>{
+router.get('/', (req, res, next)=>{ //BUSSTOP_ID라는 변수로 정류장 정보 중에서 busstop_id 보내주시면 됩니다.
     const BUSSTOP_ID = req.body.BUSSTOP_ID
     var requestUrl = `${HOST}?ServiceKey=${SERVICE_KEY}&BUSSTOP_ID=${BUSSTOP_ID}`
     request.get(requestUrl, (err, res, body)=> {
